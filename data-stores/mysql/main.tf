4d6c4912c8cc32a13084c8a77492e154a30d864c
Philip Kupsch <pkupsch@wayfair.com>
2023-01-12 11:53:35 +0100

    only set db credentials if replication is not enabled

--- data-stores/mysql/main.tf
+++ data-stores/mysql/main.tf
@@ -1,17 +1,16 @@
 resource "aws_db_instance" "ptg-db" {
     identifier_prefix = "${var.identifier_prefix}-db"
-    engine = local.engine
     allocated_storage = var.db_storage_size
     instance_class = var.db_instance_type
     skip_final_snapshot = true
-    db_name = var.db_name
-
-    #tbd in next chapter of the book, store secrets such as username and password securely somewhere, 
-    #temporarily using local envt variable - not recommended
-    username = var.db_username
-    password = var.db_password
 
     backup_retention_period = var.backup_retention_period
 
     replicate_source_db = var.replicate_source_db
+
+    #set params if replicate_source_db isn't set
+    engine = var.replicate_source_db == null ? local.engine : null
+    db_name = var.replicate_source_db == null ? var.db_name : null
+    username = var.replicate_source_db == null ? var.db_username : null
+    password = var.replicate_source_db == null ? var.db_password : null
 }
